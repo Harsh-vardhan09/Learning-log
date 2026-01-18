a0748476cb2c98ad92820a9b035115eef2a03934
Harsh-vardhan09 <harshvardhanp00@gmail.com>
2026-01-18 14:03:39 +0530

    Error handling

--- mongo3/index.js
+++ mongo3/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path=require("path");
 const Chat=require("./models/chat.js");
 const methodOverride=require("method-override");
+const ExpressError=require("./ExpressError.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -20,7 +21,7 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+  await mongoose.connect("mongodb://127.0.0.1:27017/fakeWhatsapp");
 }
  
 
@@ -28,66 +29,125 @@ app.listen(8080, () => {
   console.log("server is running on 8080");
 });
 
+//asyncWrap fuction syntax
+
+function asyncWrap(fn){
+  return function(req,res,next){
+    fn(req,res,next).catch(err=>next(err));
+  };
+}; 
+
+
 //index route
-app.get("/chats",async (req,res)=>{
-  let chats= await Chat.find();
-  res.render("index.ejs",{chats})
-  // console.log(chats)
-});
+app.get("/chats",asyncWrap(async (req,res)=>{
+    let chats= await Chat.find();
+    res.render("index.ejs",{chats})
+    // console.log(chats);
+}));
 
 //new route
 app.get("/chats/new",(req,res)=>{
+  // throw new ExpressError(401,"page not found")
   res.render("new.ejs")
 })
 
 //create route
-app.post("/chats",(req,res)=>{
-   let {from,msg,to}=req.body;
-   let newChat=new Chat({
-    from:from,
-    to:to,
-    msg:msg,
-    created_at:new Date()
-   });
-
-   //database create delete update everything is an asynchronus func so we need to use await or then.
-
-   newChat.save().then(res=>{console.log("chat was saved");}
-  ).catch(err=>{console.log(err);
-   })
-   res.redirect("/chats")
-
+app.post("/chats",asyncWrap(async(req,res,next)=>{
+      let {from,msg,to}=req.body;
+      let newChat=new Chat({
+          from:from,
+          to:to,
+          msg:msg,
+          created_at:new Date()
+      });
+      //database create delete update everything is an asynchronus func so we need to use await or then.
+      await newChat.save();
+      res.redirect("/chats")
    //where we use then we dont use await since then is considered async by js.
+}));
+
+//new -show route
+app.get("/chats/:id",async(req,res,next)=>{
+  try{
+    let {id}=req.params;
+    let chat=await Chat.findById(id);
+     if(!chat){
+      console.log("not found");
+      next(new ExpressError(404,"chat not found"));//async doesnt call next automatically so we need to explicitly call it in async func in version 4  of express. in version 5 it does call next automatically
+     }
+    res.render("edit.ejs",{chat});
+  }catch(err){
+    next(err);
+  }
 });
 
+//if we send the wrong length we get mongoose error so we should send the right length.
+
 //Edit route 
-app.get("/chats/:id/edit",async(req,res)=>{
-  let {id}=req.params;
-  let chat=await Chat.findById(id);
-  res.render("edit.ejs",{chat});
+app.get("/chats/:id/edit",async(req,res,next)=>{
+  try{
+
+    let {id}=req.params;
+    let chat=await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+  }catch(err){
+    next(err);
+  }
 });
 
 //Update route
-app.put("/chats/:id",async(req,res)=>{
+app.put("/chats/:id",async(req,res,next)=>{
+  try{
     let {id}=req.params;
     let {msg:newMsg}=req.body;
 
     let updatedChat=await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
     console.log(updatedChat);
     res.redirect("/chats")
+  }catch(err){
+    next(err);
+  }
 })
 
 //delete
 
-app.delete("/chats/:id",async(req,res)=>{
-  let {id}=req.params;
-  let deletedChat=await Chat.findByIdAndDelete(id);
-  console.log(deletedChat);
-  res.redirect("/chats")
+app.delete("/chats/:id",async(req,res,next)=>{
+  try {
+    let {id}=req.params;
+    let deletedChat=await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats")
+  } catch (err ) {
+    next(err)
+  }
+});
+
+//handling special error 
+
+const handleValidationError=(err)=>{
+  console.log("this is a validation error please follow rules");
+  console.dir(err);
+  return err;
+};
+
+
+//name of error for each differnt error we waant differnt output
+
+app.use((err,req,res,next)=>{
+   console.log(err.name);
+   if(err.name==="ValidationError"){
+      err=handleValidationError(err);
+   }
+   next(err);
 })
 
 app.get("/", (req, res) => {
   // res.render("new.ejs");
   res.send("root is working")
 });
+
+app.use((err,req,res,next)=>{
+  let{status=500,message="some error occured"}=err;
+  res.status(status).send(message);
+})
   
