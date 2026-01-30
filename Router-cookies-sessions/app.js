830a9730974a733162a61ecde3791c232c999a33
Harsh Vardhan <harsh.vardhanp0901@gmail.com>
2026-01-31 00:58:42 +0530

    sessions

---
+++ Router-cookies-sessions/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+
+// app.get("/reqcount", (req, res) => {
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count=1;
+//     }
+//   res.send(`you sent a request ${req.session.count}times`);
+// });
+
+// app.get("/test",(req,res)=>{
+//     res.send("test successful");
+// });
+
+
+app.get("/register",(req,res)=>{
+    let {name="anonymous"}=req.query;
+    req.session.name=name;
+    console.log(req.session.name);
+    res.redirect("/hello");
+});
+
+app.get("/hello",(req,res)=>{
+    res.send(`hello ${req.session.name}`);
+})
+
+app.listen("3000", () => {
+  console.log("server is running at 3000");
+});
--- Router-cookies-sessions/server.js
+++ Router-cookies-sessions/server.js
@@ -1,8 +1,10 @@
-const express=require("express");
-const app=express();
-const users=require("./routes/user.js");
-const posts=require("./routes/post.js");
-const cookieParser=require("cookie-parser");
+const express = require("express");
+const app = express();
+const users = require("./routes/user.js");
+const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 
 app.use(cookieParser("secretcode"));
 
@@ -21,11 +23,6 @@ app.get("/getcookies",(req,res)=>{
     res.send("sent you some cookies");
 });
 
-app.listen("3000",()=>{
-    console.log("server is running at 3000");
-});
-
-
 app.get("/greet",(req,res)=>{
     let {name="anonymous"}=req.cookies;
     res.send(`hello ${name}`);
@@ -40,5 +37,6 @@ app.use("/users",users);
 
 app.use("/post",posts);
 
-
-
+app.listen("3000", () => {
+  console.log("server is running at 3000");
+});
